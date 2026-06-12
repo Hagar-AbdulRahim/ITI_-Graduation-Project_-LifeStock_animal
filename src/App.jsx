b16dd58db08d9b883acff6f1f2fada1a09329633
Hagar AbdulRahim <hagarabdulrahim@gmail.com>
2026-06-12 15:03:31 +0300

    update dashboard setup and configs

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,6 @@
-// App.jsx
-import { RouterProvider } from "react-router-dom";
-import { router } from "./routes";
+import { RouterProvider } from 'react-router-dom';
+import { router } from './routes';
 
 export default function App() {
   return <RouterProvider router={router} />;
 }
-import React from 'react'
-import { BrowserRouter } from 'react-router-dom'
-import AppRoutes from './routes/AppRoutes'
-
-function App() {
-  return (
-    <BrowserRouter>
-      <AppRoutes />
-    </BrowserRouter>
-  )
-}
-
-export default App
