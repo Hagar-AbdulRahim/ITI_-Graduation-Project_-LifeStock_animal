5fa245138e45dd74135b001239b9002cc4d47030
Hagar AbdulRahim <hagarabdulrahim@gmail.com>
2026-06-14 19:10:43 +0300

    remove admin-dashboard folder

--- frontend-Dashboard/src/App.jsx
+++
@@ -1,7 +0,0 @@
-// App.jsx
-import { RouterProvider } from "react-router-dom";
-import { router } from "./routes";
-
-export default function App() {
-  return <RouterProvider router={router} />;
-}
